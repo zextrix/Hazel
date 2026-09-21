f1f5e9ef980701e82af6857dba1ac4e8b96fc984
zextrix <pratyushgawai15@gmail.com>
2026-09-21 19:26:22 -0400

    Finished the GLFW Window video

--- Hazel/src/Hazel/Application.cpp
+++ Hazel/src/Hazel/Application.cpp
@@ -4,11 +4,13 @@
 #include "Hazel/Events/ApplicationEvent.h"
 #include "Hazel/Log.h"
 
+#include <GLFW/glfw3.h>
+
 namespace Hazel {
 
 	Application::Application()
 	{
-
+		m_Window = std::unique_ptr<Window>(Window::Create());
 	}
 	
 	Application::~Application()
@@ -18,8 +20,11 @@ namespace Hazel {
 
 	void Application::Run()
 	{
-		WindowResizeEvent e(1280, 720);
-		HZ_TRACE(e.ToString());
-		while (true);
+		while (m_Running)
+		{
+			glClearColor(1, 0, 1, 1);
+			glClear(GL_COLOR_BUFFER_BIT);
+			m_Window->OnUpdate();
+		}
 	}
 }
--- Hazel/src/Hazel/Application.h
+++ Hazel/src/Hazel/Application.h
@@ -2,6 +2,7 @@
 
 #include "Core.h"
 #include "Events/Event.h"
+#include "Window.h"
 namespace Hazel {
 	class HAZEL_API Application
 	{
@@ -10,6 +11,9 @@ namespace Hazel {
 		virtual ~Application();
 
 		void Run();
+	private:
+		std::unique_ptr<Window> m_Window;
+		bool m_Running = true;
 	};
 
 
