a3579e8edae09e0bc28c53a3184732d83b99c816
zextrix <pratyushgawai15@gmail.com>
2026-09-22 15:56:04 -0400

    Finished Layers video

--- Hazel/src/Hazel/Application.h
+++ Hazel/src/Hazel/Application.h
@@ -3,6 +3,7 @@
 #include "Core.h"
 #include "Events/Event.h"
 #include "Window.h"
+#include "Hazel/LayerStack.h"
 #include "Hazel/Events/ApplicationEvent.h"
 namespace Hazel {
 	class HAZEL_API Application
@@ -14,10 +15,15 @@ namespace Hazel {
 		void Run();
 
 		void OnEvent(Event& e);
+
+		
+		void PushLayer(Layer* layer);
+		void PushOverlay(Layer* layer);
 	private:
 		bool OnWindowClose(WindowCloseEvent& e);
 		std::unique_ptr<Window> m_Window;
 		bool m_Running = true;
+		LayerStack m_LayerStack;
 	};
 
 
